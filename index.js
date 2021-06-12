ac3a22e9f443548afa8fbdedda5d799c14672f9f
diltz <takpeshev.nicolai@gmail.com>
2021-06-12 13:17:39 +0700

    1

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 /*
 
-index.js
-author: diltz (https://github.com/Diltz)
-date: 11.06.21
+ npm/rbxapi.js
+ index.js
+ author: diltz (https://github.com/Diltz)
+ date: 12.06.21
 
 */
 
-module.exports.getCSRFtoken = require("./functions/getCSRFtoken.js")
-module.exports.getCurrentUser = require("./functions/getCurrentUser.js")
-module.exports.getGroupInfo = require("./functions/getGroupInfo.js")
-module.exports.getUserInfo = require("./functions/getUserInfo.js")
-module.exports.getUserStatus = require("./functions/getUserStatus.js")
+const fs = require("fs")
+const lib_path = "./lib"
+var auth_path = lib_path + "/auth"
+var group_path = lib_path + "/group"
+var user_path = lib_path + "/user"
+
+fs.readdirSync(auth_path).forEach((value,index) => {
+    let file = value.split(".")[0]
+    module.exports[file] = require(auth_path + "/" + value)
+})
+
+fs.readdirSync(group_path).forEach((value,index) => {
+    let file = value.split(".")[0]
+    module.exports[file] = require(group_path + "/" + value)
+})
+
+fs.readdirSync(user_path).forEach((value,index) => {
+    let file = value.split(".")[0]
+    module.exports[file] = require(user_path + "/" + value)
+})
