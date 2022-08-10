a2e5bc3af9533038c7f3729a1523f6f1fec30669
Diltz <takpeshev.nicolai@gmail.com>
2022-08-11 01:43:25 +0700

    getUserIdByUsername fix

--- script.test.js
+++ script.test.js
@@ -6,4 +6,8 @@ roblox.setRank(cookie,2824810,192226872,253).then(function(){
     console.log("Success!")
 }).catch(console.error)*/
 
-roblox.setGroupStatus(cookie,2824810,"test")
+//roblox.setGroupStatus(cookie,2824810,"test")
+
+roblox.getUserIdByUsername("hdgdgdhdhdhdhdhdqgdhhddhhdhdhhd").then((value) => {
+    console.log(value)
+})
